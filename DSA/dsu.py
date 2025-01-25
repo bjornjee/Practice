60aaa1f44d5e2bcbf4b3ebcdc3a5e2b1e2ffffd9
pinkgu <bjornjee95@gmail.com>
2025-01-25 20:31:55 +0800

    chore: add usecase for dsu
    Signed-off-by: pinkgu <bjornjee95@gmail.com>

--- DSA/dsu.py
+++ DSA/dsu.py
@@ -6,6 +6,7 @@
 # We need to ensure that the height of the tree is as low as possible
 # We can do this by using path compression
 
+from typing import List, Tuple
 
 class DisjointSet:
     def __init__(self,size):
@@ -36,11 +37,33 @@ class DisjointSet:
     
     def __repr__(self):
         return f'{self.parent}\n{self.size}\n'
+    
+
+# use case:
+# use union join on edges in a undirected graph to get cycles
+def has_cycle(size: int, edges: List[Tuple[int]]):
+    dsu=DisjointSet(size)
+    for a,b in edges:
+        if dsu.connected(a,b):
+            return True
+        dsu.union(a,b)
+    return False
+
+# use case:
+# use union join on edges in a directed graph to get connected components
+def connected_components(size: int, edges: List[Tuple[int]]):
+    dsu=DisjointSet(size)
+    for a,b in edges:
+        dsu.union(a,b)
+    return [dsu.find(i) for i in range(size)]
+
 
 if __name__ =='__main__':
-    ds = DisjointSet(5)
-    ds.union(1,2)
-    ds.union(2,3)
-    print(ds.connected(1,3))
-    print(ds.connected(1,4))
-    print(ds)
+    # ds = DisjointSet(5)
+    # ds.union(1,2)
+    # ds.union(2,3)
+    # print(ds.connected(1,3))
+    # print(ds.connected(1,4))
+    # print(ds)
+    print(has_cycle(5,[(1,2),(2,3),(3,4),(3,1)]))
+    print(connected_components(5,[(1,2),(2,3),(3,1),(4,0)]))
